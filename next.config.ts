a53e4ef89104853e6ea2a9ccd788a352c507da3b
muhammed4u <muhammmed4u@gmail.com>
2026-05-22 22:07:40 +0300

    up

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  outputFileTracingIncludes: {
+    "/api/**/*": ["./prisma/dev.db"],
+    "/dashboard/**/*": ["./prisma/dev.db"],
+  },
 };
 
 export default nextConfig;
